1b9ba6720c1d6d0474e4e1987a4200083dac8ac4
Alka <Alka@192.168.2.12>
2017-08-13 10:48:46 -0400

    automation

---
+++ Demo/src/demoTest/LogIn2.java
@@ -0,0 +1,40 @@
+package demoTest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LogIn2 
+{ public WebDriver driver= null;
+	
+	@Test
+	public void LogIn1() throws IOException
+	{   Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\Alka\\workspace2\\Demo\\src\\login_details.properties");
+		prop.load(fis);
+		
+		if(prop.getProperty("Browser").contains("firefox"))
+		{
+			driver = new FirefoxDriver();
+		}
+		else if(prop.getProperty("Browser").contains("chrome"))
+		{		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Alka\\seljen\\seljen\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		}
+		
+		driver.get(prop.getProperty("url"));
+		driver.findElement(By.xpath("html/body/form/table/tbody/tr[1]/td[2]/input")).sendKeys(prop.getProperty("userid"));
+		driver.findElement(By.xpath("html/body/form/table/tbody/tr[2]/td[2]/input")).sendKeys(prop.getProperty("pass"));
+		driver.findElement(By.xpath("html/body/form/table/tbody/tr[3]/td[2]/input[1]")).click();
+		driver.quit();
+		
+	}
+
+}
